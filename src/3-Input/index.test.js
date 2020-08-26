49dbb9e47037dd1e8877a89c9eb5fa335a86d8c4
Guilherme Kuniyoshi <guilhermekuni@gmail.com>
2020-08-25 23:32:13 -0300

    feat: implements Input onChange test

--- src/3-Input/index.test.js
+++ src/3-Input/index.test.js
@@ -6,14 +6,39 @@ const inputTestId = 'input';
 
 describe('Input', () => {
   it('shoulde render', () => {
-    const { getByTestId } = render(<Input onChange={() => {}} />);
+    const { getByTestId } = render(<Input onChange={() => { }} />);
     expect(getByTestId(inputTestId)).toBeInTheDocument();
-  })
+  });
 
   it('should render with initial value', () => {
     const testValue = 'Test Value';
-    const { getByTestId } = render(<Input value={testValue} onChange={() => {}} />);
+    const { getByTestId } = render(<Input value={testValue} onChange={() => { }} />);
 
     expect(getByTestId(inputTestId).value).toBe(testValue);
-  })
+  });
+
+  it('should change value', () => {
+    let testValue = 'Test 1';
+
+    const changeValue = (value) => {
+      testValue = value;
+    }
+
+    const { getByTestId, rerender } = render(
+      <Input
+        value={testValue}
+        onChange={(e) => changeValue(e.target.value)}
+      />
+    );
+
+    const input = getByTestId(inputTestId);
+
+    expect(input.value).toBe('Test 1');
+
+    fireEvent.change(input, { target: { value: 'Test 2' } });
+
+    rerender(<Input value={testValue} onChange={(e) => changeValue(e.target.value)} />);
+
+    expect(input.value).toBe('Test 2');
+  });
 });
